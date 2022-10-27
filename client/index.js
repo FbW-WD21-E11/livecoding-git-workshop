ef5fc545b41319a8ea9dc44bef0f41af08d2e708
Gilles <gilles.vanhaecke-freelancer@digitalcareerinstitute.org>
2022-10-27 10:11:16 +0200

    adding login functionality

--- client/index.js
+++ client/index.js
@@ -3,3 +3,7 @@ const parameter = "This is a parameter";
 const test = () => {
     console.log("This is some test functionality");
 }
+
+const login = () => {
+    console.log("This is some new login functionality");
+}
